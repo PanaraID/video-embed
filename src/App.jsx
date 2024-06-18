da1b785fe95527d4a3f5c6063d8f05e2fb23da3e
ahlaiptek <iptekahla@gmail.com>
2024-06-18 12:53:38 +0700

    Menambahkan video

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <iframe src="https://drive.google.com/file/d/116I6DUpFunx-e8WLrqs1PyHjMmwTPeAg/preview"></iframe>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
