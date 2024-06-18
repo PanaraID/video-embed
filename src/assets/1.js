708e7756533e46dbf224e9029b1a03eea4597002
ahlaiptek <iptekahla@gmail.com>
2024-06-18 16:14:32 +0700

    cemungut

---
+++ src/assets/1.js
@@ -0,0 +1,4 @@
+// JavaScript
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Mencegah menu konteks muncul saat klik kanan
+});
--- src/pages/Satu.jsx
+++ src/pages/Satu.jsx
@@ -1,7 +1,15 @@
+import { Fragment } from 'react'
+import '../assets/1.css'
+import '../assets/1.js'
+
 const Satu = () => {
-  return <iframe
-    src={import.meta.env.VITE_DRIVE}
-  />
+  return <Fragment>
+    <h6>Disable Klik Kanan</h6>
+    <iframe
+      src={import.meta.env.VITE_DRIVE}
+    />
+    <p>Tetap tidak bisa, sebab ikon itu selalu muncul</p>
+  </Fragment>
 }
 
 export default Satu
